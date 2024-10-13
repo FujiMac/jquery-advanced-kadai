5af4ef44ae9df3434dfa4cab3737eb9a196f3fe3
FujiMac <165458514+FujiMac@users.noreply.github.com>
2024-10-13 12:25:34 +0900

    kadai

---
+++ kadai_tutorial_output/script.js
@@ -0,0 +1,141 @@
+$(function(){
+  $('.button-more').on('mouseover',function(){
+    $(this).animate({
+      opacity:0.5,
+      marginLeft:20,
+    },100);
+  });
+  $('.button-more').on('mouseout',function(){
+    $(this).animate({
+      opacity:1.0,
+      marginLeft:0,
+    },100);
+  });
+  //カルーセル
+  $('.carousel').slick({
+    autoplay:true,
+    dots:true,
+    infinite:true,
+    autoplaySpeed:5000,
+    arrows:false,
+  });
+
+  $('#submit').on('click',function(event){
+      event.preventDefault();
+      let result = inputCheck()
+    //エラー判定とメッセージを取得
+    let error = result.error;
+    let message = result.message;
+
+    //エラーがなかったらフォームを送信する
+    if(error==false){
+      //送信成功メッセージのみ表示
+      alert('お問い合わせを送信しました。')
+    } else {
+      //エラーメッセージを表示
+      alert(message);
+    }
+  });
+
+  $('#name').blur(function(){
+    inputCheck();
+  });
+  $('#furigana').blur(function(){
+    inputCheck();
+  });
+  $('#email').blur(function(){
+    inputCheck();
+  });
+  $('#tel').blur(function(){
+    inputCheck();
+  });
+  $('#message').blur(function(){
+    inputCheck();
+  });
+  $('#agree').click(function(){
+    inputCheck();
+  });
+
+  function inputCheck(){
+    let result;
+  
+    let message='';
+
+    let error = false;
+
+    //お名前のチェック
+    if($('#name').val()==''){
+      //エラーあり
+      $('#name').css('background-color','#f79999');
+      error = true;
+      message += 'お名前を入力してください。\n';
+    } else {
+      //エラーなし
+      $('#name').css('background-color','#fafafa');
+    }
+
+    //フリガナのチェック
+    if($('#furigana').val()==''){
+      //エラーあり
+      $('#furigana').css('background-color','#f79999');
+      error = true;
+      message += 'フリガナを入力してください。\n';
+    } else {
+      //エラーなし
+      $('#furigana').css('background-color','#fafafa');
+    }  
+
+    //お問い合わせのチェック
+    if($('#message').val()==''){
+      //エラーあり
+      $('#message').css('background-color','#f79999');
+      error = true;
+      message += 'お問い合わせ内容を入力してください。\n';
+    } else {
+      //エラーなし
+      $('#message').css('background-color','#fafafa');
+    }  
+
+    //メールアドレスのチェック
+    if($('#email').val() == '' || $('#email').val().indexOf('@') == -1 || $('#email').val().indexOf('.') == -1) {
+      //エラーあり
+      $('#email').css('background-color','#f79999');
+      error = true;
+      message += 'メールアドレスが未記入、または「@」「.」が含まれていません。\n';
+    } else {
+      //エラーなし
+      $('#email').css('background-color','#fafafa');
+    }
+    
+    //電話番号のチェック
+    if($('#tel').val() != '' && $('#tel').val().indexOf('-') == -1) {
+      //エラーあり
+      $('#tel').css('background-color','#f79999');
+      error = true;
+      message += '電話番号に「-」が含まれてません。\n';
+    } else {
+      //エラーなし
+      $('#tel').css('background-color','#fafafa');
+    } 
+
+    //個人情報のチェックボックスのチェック
+    if($('#agree').prop('checked')==false){
+      error = true;
+      message += '個人情報の取り扱いについてご同意いただける場合は、チェックボックスにチェックしてください。\n';
+    }
+
+    //エラーの有無で送信ボタンを切り替え
+    if(error == true){
+      $('#submit').attr('src','images/button-submit.png');
+    } else {
+      $('#submit').attr('src','images/button-submit-blue.png');   
+    }
+
+    //オブジェクトでエラー判定とメッセージを返す
+    result = {
+      error:error,
+      message: message
+    }
+    return result;
+  }
+});
